e0190934ece59c9ccfc53c2b74cb84eef9377b38
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-02-11 11:31:24 +0100

    Added a specific view

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from bottle import get, run
+from bottle import get, run, view
 
 ##############################
 @get("/")
+@view("index")
 def _():
-    return "Hello world"
+    return
 ##############################
 
 run(host="127.0.0.1", port=3333, debug=True, reloader=True, server="paste")
