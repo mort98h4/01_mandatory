263698e53308dbe8d36db188444f1666e829e055
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-02-12 11:56:41 +0100

    Create logout_get.py

---
+++ logout_get.py
@@ -0,0 +1,11 @@
+from bottle import get, redirect, request
+import jwt
+import g
+
+@get("/logout")
+def _():
+    encoded_jwt = request.get_cookie("jwt")
+    decoded_jwt = jwt.decode(encoded_jwt, g.JWT_SECRET, algorithms=["HS256"])
+    user_session_id = decoded_jwt["user_session_id"]
+    g.SESSIONS.remove(user_session_id)
+    return redirect("/")
