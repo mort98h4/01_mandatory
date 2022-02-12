4fcfaf3697226b5fcbfb8720a075c65ce57db15e
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-02-12 13:38:20 +0100

    Created update_post_post.py which updates post with hardcoded value

---
+++ update_post_post.py
@@ -0,0 +1,12 @@
+from bottle import post, redirect, request
+import g
+
+@post("/update-post")
+def _():
+    post_id = request.forms.get("post_id")
+    if post_id:
+        for index, post in enumerate(g.POSTS):
+            if post["id"] == post_id:
+                post["post_content"] = "yyy"
+    
+    return redirect("/feed")
