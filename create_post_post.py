cd242a3e9782897a5292eae85c9fc60b677ff3f5
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-02-12 15:58:37 +0100

    Posts now have a date

--- create_post_post.py
+++ create_post_post.py
@@ -2,18 +2,21 @@ from bottle import post, redirect, request
 import g
 import uuid
 import jwt
+import time
 
 @post("/create-post")
 def _():
     encoded_jwt = request.get_cookie("jwt")
     decoded_jwt = jwt.decode(encoded_jwt, g.JWT_SECRET, algorithms=["HS256"])
     post_content = request.forms.get("post_content")
+    post_date = time.strftime('%d-%m-%Y %H:%M:%S', time.localtime(int(time.time())))
 
     post = {
         "id": str(uuid.uuid4()),
         "user_first_name": decoded_jwt["user_first_name"],
         "user_email": decoded_jwt["user_email"],
-        "post_content": post_content
+        "post_content": post_content,
+        "post_date": post_date
     }
     g.POSTS.append(post)
 
--- g.py
+++ g.py
@@ -1,3 +1,5 @@
+import time
+
 USERS = [
     {
         "user_first_name": "Morten",
@@ -8,9 +10,9 @@ USERS = [
 ]
 SESSIONS = []
 POSTS = [
-    {"id": "a", "user_first_name": "a", "user_email": "a@a.dk", "post_content": "xxx"},
-    {"id": "b", "user_first_name": "b", "user_email": "b@b.dk", "post_content": "xxx"},
-    {"id": "c", "user_first_name": "c", "user_email": "c@c.dk", "post_content": "xxx"},
+    {"id": "a", "user_first_name": "a", "user_email": "a@a.dk", "post_content": "xxx", "post_date": time.strftime('%d-%m-%Y %H:%M:%S', time.localtime(1644672386))},
+    {"id": "b", "user_first_name": "b", "user_email": "b@b.dk", "post_content": "xxx", "post_date": time.strftime('%d-%m-%Y %H:%M:%S', time.localtime(1643677386))},
+    {"id": "c", "user_first_name": "c", "user_email": "c@c.dk", "post_content": "xxx", "post_date": time.strftime('%d-%m-%Y %H:%M:%S', time.localtime(1643672386))}
 ]
 REGEX_EMAIL = '^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
 REGEX_PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
