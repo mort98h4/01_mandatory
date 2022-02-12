49deb606c4325ce2e8cbe03ea127719bf6b945f3
Morten Gross <61137926+mort98h4@users.noreply.github.com>
2022-02-12 12:00:22 +0100

    gets jwt from cookie and decodes it. checks if user is in sessions

--- feed_get.py
+++ feed_get.py
@@ -1,6 +1,13 @@
-from bottle import get, view
+from bottle import get, view, redirect, request
+import g
+import jwt
 
 @get("/feed")
 @view("feed")
 def _():
+    encoded_jwt = request.get_cookie("jwt")
+    decoded_jwt = jwt.decode(encoded_jwt, g.JWT_SECRET, algorithms=["HS256"])
+    user_session_id = decoded_jwt["user_session_id"]
+    if user_session_id not in g.SESSIONS:
+        return redirect("/login")
     return
